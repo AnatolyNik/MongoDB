fc61a93bd183eae1094cde10fa3be043bf4f8736
AnatolyNik <a_nik@hotmail.com>
2017-11-15 18:54:31 +0300

    Start of work with MongoDB from R

---
+++ R-MongoDB.R
@@ -0,0 +1,12 @@
+# Connect to MongoDB
+# http://127.0.0.1:29123/library/RMongo/html/RMongo-package.html
+library(RMongo)
+
+# Connect to a mongodb database: 
+ mongo <- mongoDbConnect("test", "localhost", 27017) 
+ 
+# Insert a document into a collection: This will insert a document of '"foo":"bar"' into the "test_data" collection.  
+ output <- dbInsertDocument(mongo, "test_data", '{"foo": "bar"}')
+
+# Close the connection: 
+dbDisconnect(mongo)
